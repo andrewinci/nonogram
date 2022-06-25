4ed3b81dd4ffd32099cdc66c74ebf2972e942205
andrewinci <andrewinci@gmail.com>
2022-06-24 20:55:13 -0400

    generate random game

--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,29 @@ const sample: Nonogram = {
 }
 
 class NonogramGame {
+    private generateHeader(cells: boolean[]) {
+        let header = [0]
+        for (const c of cells) {
+            if (c) header[header.length - 1] += 1
+            else if (header[header.length - 1] != 0) header.push(0)
+        }
+        // remove last 0 if any
+        if (header[header.length - 1] == 0) header.pop()
+        return header
+    }
     generate(size: number): Nonogram {
         const res = Array(size).fill(0).map(() => Array(size).fill(0).map(() => this.randBoolean()))
+        const rowHeader = res.map(row => this.generateHeader(row))
+        const colHeader = []
+
+        for (let i = 0; i < res.length; i++) {
+            colHeader.push(this.generateHeader(res.map(row => row[i])))
+        }
+
         return {
             body: res.map(r => r.map(c => c ? CellStatus.Full : CellStatus.Empty)),
-            rowHeader: Array(size).fill(0).map(() => [0]),
-            columnHeader: Array(size).fill(0).map(() => [0])
+            rowHeader: rowHeader,
+            columnHeader: colHeader
         }
     }
 
